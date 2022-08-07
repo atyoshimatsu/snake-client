36b76dcdd022ae0eeb369dc76886749b0210583a
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-07 17:41:12 +0000

    Add connect event handler

--- clinet.js
+++ clinet.js
@@ -1,4 +1,4 @@
-const net = require("net");
+const net = require('net');
 
 // establishes a connection with the game server
 const connect = function() {
@@ -8,12 +8,18 @@ const connect = function() {
   });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding('utf8');
 
   // display incoming data
-  conn.on("data", (data) => {
+  conn.on('data', (data) => {
     console.log(data);
   });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+    conn.write('Name: ATY');
+  });
+
   return conn;
 };
 
